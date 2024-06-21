e3974eda59d3b0ad3859f876a99989b3c071be3a
Stiven Fortes <stiven.fortes@devgo.org>
2024-06-21 19:06:33 -0100

    feat: finish basic upload and classifier

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import os
 
 app = Flask(__name__)
+model = load_model('model.h5')
 
-@app.route('/')
-def Home():
-    return "Hello Flask"
+def predict_image(img_path, model):
+    img = image.load_img(img_path, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0) / 255.0
+    prediction = model.predict(img_array)
+    return 'Dog' if prediction[0] > 0.5 else 'Cat'
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file_path = os.path.join('uploads', file.filename)
+            file.save(file_path)
+            result = predict_image(file_path, model)
+            os.remove(file_path)
+            return render_template('predicted.html', result=result)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
